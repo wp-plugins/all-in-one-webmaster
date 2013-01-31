3fa776bbb5ed310af1d48443225a3ed4992b7006
iCrunch <iCrunch@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-31 01:40:34 +0000

    Major code upgrade + Admin Page redesigned + Handy tips added. Version bump 8.2.
    git-svn-id: https://plugins.svn.wordpress.org/all-in-one-webmaster/tags/8.2@661469 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/setting-page/aiow-footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author iCrunch.co
+ * Plugin: All in One Webmaster
+ */
+?>
+
+
+
+<div class="clear">
+    <p>
+        <br/>&copy; Copyright 2007 - <?php echo date("Y"); ?> <a href="http://iCrunch.co" target="_blank">iCrunch.co</a>
+    </p>
+</div>		
---
+++ js/myscript.js
@@ -0,0 +1,50 @@
+jQuery.noConflict();
+
+jQuery(function () {
+    jQuery("#aiowtabs").tabs();
+});
+
+jQuery(function() {
+	jQuery('#aioweast1').powerTip({placement: 'e'});
+});
+
+jQuery(function() {
+	jQuery('#aioweast2').powerTip({placement: 'e'});
+});
+
+jQuery(function() {
+	jQuery('#aioweast3').powerTip({placement: 'e'});
+});
+
+jQuery(function() {
+	jQuery('#aioweast4').powerTip({placement: 'e'});
+});
+
+jQuery(function() {
+	jQuery('#aioweast5').powerTip({placement: 'e'});
+});
+
+jQuery(function() {
+	jQuery('#aioweast6').powerTip({placement: 'e'});
+});
+
+jQuery(function() {
+	jQuery('#aioweast7').powerTip({placement: 'e'});
+});
+
+jQuery(function() {
+	jQuery('#aioweast8').powerTip({placement: 'e'});
+});
+
+jQuery(function() {
+	jQuery('#aioweast9').powerTip({placement: 'e'});
+});
+
+jQuery(function() {
+	jQuery('#aioweast10').powerTip({placement: 'e'});
+});
+
+jQuery(function() {
+	jQuery('#aioweast11').powerTip({placement: 'e'});
+});
+
