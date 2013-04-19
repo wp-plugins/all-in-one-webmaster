6da83d2d9b09aedc7497d9b1b21d1b0a2143243c
Crunchify <Crunchify@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-19 22:28:20 +0000

    Fixed a cross scripting security issue (props Charlie Eriksen via Secunia SVCRP)
    git-svn-id: https://plugins.svn.wordpress.org/all-in-one-webmaster/tags/8.2.4@700503 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/setting-page/aiow-footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Crunchify.com
+ * Plugin: All in One Webmaster
+ */
+?>
+
+
+
+<div class="clear">
+    <p>
+        <br/>&copy; Copyright 2012 - <?php echo date("Y"); ?> <a href="http://Crunchify.com" target="_blank">Crunchify.com</a>
+    </p>
+</div>		
