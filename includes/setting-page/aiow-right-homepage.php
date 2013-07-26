970b93ac270050908edf0692996dc31f22aab570
Crunchify <Crunchify@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-26 22:56:42 +0000

    = 8.2.8 = * Updated Compete Analytics Code. Thanks to www.compete.com team for updated code snippet.
    git-svn-id: https://plugins.svn.wordpress.org/all-in-one-webmaster/tags/8.2.8@746906 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/setting-page/aiow-right-homepage.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author Crunchify.com
+ * Plugin: All in One Webmaster
+ */
+?>
+
+<br>
+<div id="side-info-column" class="inner-sidebar">
+    <div class="postbox">
+        <h3 class="hndle"><span>All in One Webmaster</span></h3>
+
+        <div class="inside">
+            <ul>
+                <li><a href="http://Crunchify.com/all-in-one-webmaster/" title="All in One Webmaster" target="_blank">Plugin
+                    Homepage</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
