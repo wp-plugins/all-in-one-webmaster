97adb764f6f5caae91a8c64a598bf59cb4393b82
Crunchify <Crunchify@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-14 19:23:21 +0000

    open link in new tab.
    git-svn-id: https://plugins.svn.wordpress.org/all-in-one-webmaster/trunk@772382 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/setting-page/aiow-right-homepage.php
+++ includes/setting-page/aiow-right-homepage.php
@@ -13,7 +13,7 @@
         <div class="inside">
             <ul>
                 <div align="center">
-<a href="https://pro.crunchify.com/all-in-one-webmaster-premium/"><img class="alignnone size-full wp-image-290 aligncenter" alt="Buy All in One Webmaster Premium Now" src="https://pro.crunchify.com/wp-content/uploads/2013/08/Buy-All-in-One-Webmaster-Premium-Now.png" width="181" height="81" /></a>
+<a href="https://pro.crunchify.com/all-in-one-webmaster-premium/" target="_blank"><img class="alignnone size-full wp-image-290 aligncenter" alt="Buy All in One Webmaster Premium Now" src="https://pro.crunchify.com/wp-content/uploads/2013/08/Buy-All-in-One-Webmaster-Premium-Now.png" width="181" height="81" /></a>
 </div>
             </ul>
         </div>
